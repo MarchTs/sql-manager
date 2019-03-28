b885dbfaa548525f4c0cb7c991868a89f6402d30
Thanapat Saisoontornwattana <march_ts@outlook.com>
2019-03-28 17:37:49 +0700

    initial

---
+++ sql-manager.js
@@ -0,0 +1,40 @@
+const sql = require("mssql");
+const config = require("build-variance");
+const logManager = require("log-manager");
+
+const poolLog = new sql.ConnectionPool(config.configSql, error => {
+    if (error !== null) console.log("connect to log db error: ", error);
+});
+
+class SQLManager {
+    constructor() {}
+
+    async executeStore(store, storeParams) {
+        let startDate = new Date();
+        // console.log("executing store:", store);
+        await poolLog;
+        let request = new sql.Request(poolLog);
+        if (storeParams) {
+            storeParams.forEach(param => {
+                request.input(
+                    param.name,
+                    param.type || sql.NVarChar,
+                    param.value
+                );
+            });
+        }
+        let result = await request.execute(store);
+        // console.log("executed store:", store);
+
+        logManager.insertDBLog(
+            store,
+            storeParams,
+            result.recordsets,
+            config.configSql,
+            new Date() - startDate
+        );
+        return result;
+    }
+}
+const sqlManager = new SQLManager();
+module.exports = sqlManager;
